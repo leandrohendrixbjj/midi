95d820ba23a25c03b185e38468715d80c66775e6
leandrohendrixbjj <leandrotully@gmail.com>
2022-08-22 18:54:10 -0300

    Lista de elementos

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ function playPom(){
     document.querySelector("#som_tecla_pom").play();
 }
 
-document.querySelector(".tecla_pom").onclick = playPom;
+//document.querySelector(".tecla_pom").onclick = playPom;
+
+let list = document.querySelectorAll('.tecla');
+
+list.forEach(l => {
+    console.log(l);
+});
 
 
