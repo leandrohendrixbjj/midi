89207e4ab117d7774fe76b02ea1dce33844cf17b
leandrohendrixbjj <leandrotully@gmail.com>
2022-10-27 20:39:25 -0300

    Textos dinâmicos

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 function tocaSom(idElementoAudio) {
-  console.log(idElementoAudio.path[0])
-  //document.getElementById(idElementoAudio).play();
+  document.querySelector(idElementoAudio).play();
 }
 
 let keyboardList = document.querySelectorAll('.tecla');
+
 let cont = 0;
-
-
 while (cont < keyboardList.length) {
-  keyboardList[cont].onclick = tocaSom;
+
+  let btnClassName = keyboardList[cont].classList[1];
+
+  keyboardList[cont].onclick = () => {
+    tocaSom(`#som_${btnClassName}`);
+  }
   cont++;
 }
