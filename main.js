3ab179634da44b598d83c39da3f4c11149c656b2
leandrohendrixbjj <leandrotully@gmail.com>
2022-10-31 18:39:15 -0300

    Adicionando e removendo classe

--- main.js
+++ main.js
@@ -4,23 +4,24 @@ function tocaSom(idElementoAudio) {
 
 let keyboardList = document.querySelectorAll('.tecla');
 
-let cont = 0;
-while (cont < keyboardList.length) {
-
-  let btnClassName = keyboardList[cont].classList[1];
-
-  keyboardList[cont].onclick = () => {
-    tocaSom(`#som_${btnClassName}`);
-  }
-  cont++;
-}
 for (let row in keyboardList) {
-  if (Number.isInteger(row)) {
-    let btnClassName = keyboardList[row].classList[1];
+
+  let tecla = keyboardList[row];
+
+  if (Number.isInteger(parseInt(row))) {
+    let btnClassName = keyboardList[row].classList['1'];
 
     keyboardList[row].onclick = () => {
+      console.log(btnClassName);
       tocaSom(`#som_${btnClassName}`);
     }
   }
-}
 
+  tecla.onkeydown = () => {
+    tecla.classList.add('ativa');
+  }
+
+  tecla.onkeydown = () => {
+    tecla.classList.remove('ativa');
+  }
+}
